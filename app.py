30236917e4383740d526ce6fb6d3c905efb11ff8
SamuelBrand97 <samuelbrand130211@gmail.com>
2022-06-27 21:45:26 -0500

    upgrade

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ st.image(image)
 st.title('Dummy Front End')
 st.text('This is a Front End for Team 88')
 
-st.sidebar.title('Navigation')
+st.sidebar.title('Data Management')
 data1 = st.sidebar.file_uploader('Upload the database here')
 
-if data1:
+data2 = st.sidebar.file_uploader('Upload the validation data')
+
+if data1 and data2:
     df = pd.read_csv(data1, encoding = 'latin-1', skiprows = 1)
     st.header('Data Header')
     st.write(df.head(10))
