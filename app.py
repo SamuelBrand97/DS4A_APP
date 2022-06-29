8ea08ddf586898340a7627190731454b8f8caf78
SamuelBrand97 <samuelbrand130211@gmail.com>
2022-06-28 22:28:56 -0500

    upgrade

--- app.py
+++ app.py
@@ -18,11 +18,35 @@ data2 = st.sidebar.file_uploader('Upload the validation data')
 
 if data1 and data2:
     df = pd.read_csv(data1, encoding = 'latin-1', skiprows = 1)
-    df =df.replace('NaN',' ')
+
+    df['APE2_VAL'].fillna('', inplace = True)
+    df['APE1_VAL'].fillna('', inplace = True)
+    df['NOM2_VAL'].fillna('', inplace = True)
+    df['NOM1_VAL'].fillna('', inplace = True)
+
     df['FULL_NAME'] = df['NOM1_VAL'] + ' ' + df['NOM2_VAL'] + ' ' + df['APE1_VAL'] + ' ' + df['APE2_VAL']
+
+    df['FULL_NAME'] = df['FULL_NAME'].str.upper() 
+
+    df['FULL_NAME'] = df.FULL_NAME.str.replace('¥', 'N')
 
 
     df1 = pd.read_csv(data2, encoding = 'latin-1', skiprows = 1)
     
-    st.header('Data Header')
+    df1['APE2_LOTE'].fillna('', inplace = True)
+    df1['APE1_LOTE'].fillna('', inplace = True)
+    df1['NOM2_LOTE'].fillna('', inplace = True)
+    df1['NOM1_LOTE'].fillna('', inplace = True)
+
+    df1['FULL_NAME'] = df1['NOM1_LOTE'] + ' ' + df1['NOM2_LOTE'] + ' ' + df1['APE1_LOTE'] + ' ' + df1['APE2_LOTE']
+
+    df1['FULL_NAME'] = df1['FULL_NAME'].str.upper() 
+
+    df1['FULL_NAME'] = df1.FULL_NAME.str.replace('¥', 'N')
+
+
+    st.header('Data Header Database')
     st.write(df.head(50))
+
+    st.header('Data Header Validation')
+    st.write(df1.head(50))
